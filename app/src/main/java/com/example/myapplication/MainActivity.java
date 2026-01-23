ac41b5ffde85d0b10a53addb6d192c192b072bcc
geckoFooBar <mudholkarrohan23@gmail.com>
2026-01-23 15:51:47 +0530

    Change UI

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
     TextView txtMessage;
     Button btnClick;
@@ -18,9 +20,28 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
-        txtMessage = findViewById(R.id.txtMessage);
-        btnClick = findViewById(R.id.btnClick);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        btnClick.setOnClickListener(view -> txtMessage.setText("Hello to TYBBA CA students!!!"));
+        txtMessage = findViewById(R.id.txtMessage);
+        // btnClick = findViewById(R.id.btnClick);
+
+        // btnClick.setOnClickListener(view -> txtMessage.setText("Hello to TYBBA CA students!!!"));
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+                if (itemId == R.id.nav_home) {
+                    txtMessage.setText("Home");
+                    return true;
+                } else if (itemId == R.id.nav_services) {
+                    txtMessage.setText("Services");
+                    return true;
+                } else if (itemId == R.id.nav_profile) {
+                    txtMessage.setText("Profile");
+                    return true;
+                } else {
+                    return false;
+                }
+        });
     }
+
 }
