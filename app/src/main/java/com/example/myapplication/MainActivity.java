ce2c2381b71cfca34c6bcd3f6a4ddeba60b0c046
geckoFooBar <mudholkarrohan23@gmail.com>
2026-02-02 20:08:11 +0530

    Add Pages

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,9 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.gms.maps.MapView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -21,31 +24,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        //mapView = findViewById(R.id.mapView);
         txtMessage = findViewById(R.id.txtMessage);
-        // btnClick = findViewById(R.id.btnClick);
-        // btnClick.setOnClickListener(view -> txtMessage.setText("Hello to TYBBA CA students!!!"));
 
+        // Replace the old NavController line with this:
+        // Replace the crashy lines (31-34) with this:
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragment_container);
 
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-            int itemId = item.getItemId();
-            if (itemId == R.id.nav_home) {
-                txtMessage.setText("Home");
-                return true;
-            } else if(itemId == R.id.nav_fav) {
-                txtMessage.setText("Favorite");
-                return true;
-            } else if (itemId == R.id.nav_search) {
-                txtMessage.setText("Search");
-                return true;
-            } else if (itemId == R.id.nav_profile) {
-                txtMessage.setText("Profile");
-                return true;
-            } else {
-                return false;
-            }
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+            NavigationUI.setupWithNavController(bottomNavigationView, navController);
 
-        });
+            bottomNavigationView.setOnItemSelectedListener(item -> {
+                boolean handled = NavigationUI.onNavDestinationSelected(item, navController);
+                if (handled) {
+                    int itemId = item.getItemId();
+                    if (itemId == R.id.nav_home) {
+                        txtMessage.setText("Home");
+                    } else if (itemId == R.id.nav_fav) {
+                        txtMessage.setText("Favorite");
+                    } else if (itemId == R.id.nav_search) {
+                        txtMessage.setText("Search");
+                    } else if (itemId == R.id.nav_profile) {
+                        txtMessage.setText("Profile");
+                    }
+                }
+                return handled;
+            });
+        } else {
+            // This logs a message to your console so you know the ID is still wrong
+            android.util.Log.e("MainActivity", "Error: fragment_container not found in XML");
+        }
     }
 
 }
